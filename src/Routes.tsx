42d453b047167a6a542cad97538a0dbec08e9915
Andy Hook <hello@andy-hook.co.uk>
2020-10-08 10:58:33 +0100

    Setup: Adjust routing (#4)

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import { Redirect, Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import GetStarted from './components/GetStarted/GetStarted'
 import Migrate from './components/Migrate/Migrate'
 
 export default function Routes(): JSX.Element {
   return (
     <Switch>
-      <Redirect exact from="/" to="/get-started" />
-      <Route exact path="/get-started" component={GetStarted} />
+      <Route exact path="/" component={GetStarted} />
       <Route exact path="/migrate" component={Migrate} />
-      <Redirect to="/get-started" />
     </Switch>
   )
 }
