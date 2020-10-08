0b2c49eb6b52f30177c9d788d8221c31124725d8
Andy Hook <hello@andy-hook.co.uk>
2020-10-08 12:16:46 +0100

    Use wallet provider

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,19 @@ import { LayoutProvider } from '@aragon/ui'
 import MainView from './components/MainView'
 import Routes from './Routes'
 import { breakpoints } from './style/breakpoints'
+import { WalletProvider } from './providers/Wallet'
 
 function App(): JSX.Element {
   return (
-    <LayoutProvider breakpoints={breakpoints}>
-      <Router>
-        <MainView>
-          <Routes />
-        </MainView>
-      </Router>
-    </LayoutProvider>
+    <WalletProvider>
+      <LayoutProvider breakpoints={breakpoints}>
+        <Router>
+          <MainView>
+            <Routes />
+          </MainView>
+        </Router>
+      </LayoutProvider>
+    </WalletProvider>
   )
 }
 
