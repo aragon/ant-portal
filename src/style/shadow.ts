79b613f468891054e79a29f1785619ca9e853307
Andy Hook <hello@andy-hook.co.uk>
2020-10-13 01:10:23 +0100

    Add branded button

--- src/style/shadow.ts
+++ src/style/shadow.ts
@@ -3,9 +3,9 @@ import { rgba } from 'polished'
 const shadowTint = '#1a2130'
 
 export const shadowDepth = {
-  close: `0px 1px 2px ${rgba(shadowTint, 0.1)}, 0px 4px 10px ${rgba(
+  close: `0px 1px 2px ${rgba(shadowTint, 0.1)}, 0px 2px 20px ${rgba(
     shadowTint,
-    0.03
+    0.05
   )}`,
   medium: `0px 2px 4px ${rgba(shadowTint, 0.03)}, 0px 10px 20px ${rgba(
     shadowTint,
--- src/style/theme.ts
+++ src/style/theme.ts
@@ -1,6 +1,9 @@
 export const theme = {
   _name: 'aragon-migrate',
   _appearance: 'light',
+  accent: '#00c2ff',
+  accentStart: '#01e8f7',
+  accentEnd: '#00c2ff',
   positive: '#38ccb6',
   positiveSurface: '#ecfaf8',
   negative: '#ff7163',
