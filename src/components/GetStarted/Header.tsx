2bf1e712c8c9b667906cea39928aed7732092246
delfipolito <delfinapolito@gmail.com>
2020-10-14 10:03:49 -0300

    Review suggestions

--- src/components/GetStarted/Header.tsx
+++ src/components/GetStarted/Header.tsx
@@ -2,7 +2,9 @@ import React, { useCallback } from 'react'
 import { useHistory } from 'react-router-dom'
 // @ts-ignore
 import { GU, IconArrowRight, useTheme, useViewport } from '@aragon/ui'
+import { fontWeight } from '../../style/font'
 import BrandButton from '../BrandButton/BrandButton'
+import LayoutLimiter from '../Layout/LayoutLimiter'
 
 function Header(): JSX.Element {
   const history = useHistory()
@@ -15,57 +17,59 @@ function Header(): JSX.Element {
   }, [history])
 
   return (
-    <div
-      css={`
-        padding: ${10 * GU}px ${2 * GU}px;
-        width: 100%;
-        text-align: center;
-        max-width: ${143 * GU}px;
-        margin: auto;
-      `}
-    >
-      <h3
+    <LayoutLimiter>
+      <div
         css={`
-          font-weight: 500;
-          font-size: 26px;
-          background: -webkit-linear-gradient(
-            88.01deg,
-            #01c4ff -19.78%,
-            #01ddfa 109.13%
-          );
-          -webkit-background-clip: text;
-          -webkit-text-fill-color: transparent;
-          margin-bottom: ${1.5 * GU}px;
+          padding: ${10 * GU}px ${2 * GU}px;
+          width: 100%;
+          text-align: center;
         `}
       >
-        Aragon Migrate
-      </h3>
-      <h1
-        css={`
-          font-weight: bold;
-          line-height: 1.2;
-          font-size: 52px;
-          margin-bottom: ${2.5 * GU}px;
-        `}
-      >
-        Migrate to ANT v2
-      </h1>
-      <p
-        css={`
-          font-weight: 500;
-          font-size: ${compactMode ? `20` : `26`}px;
-          color: ${theme.surfaceIcon};
-          margin-bottom: ${3 * GU}px;
-        `}
-      >
-        Use Aragon Migrate system to upgrade your ANT balance to the newest
-        version of the token contract. Connect your wallet to view the available
-        migrations on your account.
-      </p>
-      <BrandButton mode="strong" onClick={handleNavigateToConverter}>
-        Migrate ANT v1 <IconArrowRight /> ANT v2
-      </BrandButton>
-    </div>
+        <h3
+          css={`
+            font-weight: ${fontWeight.medium};
+            font-size: 26px;
+            background: -webkit-linear-gradient(
+              88.01deg,
+              ${theme.accentStart} -19.78%,
+              ${theme.accentEnd} 109.13%
+            );
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+            margin-bottom: ${1.5 * GU}px;
+          `}
+        >
+          Aragon Migrate
+        </h3>
+        <h1
+          css={`
+            font-weight: ${fontWeight.bold};
+            line-height: 1.2;
+            font-size: 52px;
+            margin-bottom: ${2.5 * GU}px;
+          `}
+        >
+          Migrate to ANT v2
+        </h1>
+        <p
+          css={`
+            font-weight: ${fontWeight.medium};
+            font-size: ${compactMode ? `20` : `26`}px;
+            color: ${theme.surfaceContentSecondary};
+            margin: auto;
+            margin-bottom: ${3 * GU}px;
+            max-width: ${143 * GU}px;
+          `}
+        >
+          Use Aragon Migrate system to upgrade your ANT balance to the newest
+          version of the token contract. Connect your wallet to view the
+          available migrations on your account.
+        </p>
+        <BrandButton mode="strong" onClick={handleNavigateToConverter}>
+          Migrate ANT v1 <IconArrowRight /> ANT v2
+        </BrandButton>
+      </div>
+    </LayoutLimiter>
   )
 }
 
