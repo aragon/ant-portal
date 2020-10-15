8dddc09b5d4a5a10b17087cf7738c75eb50e3b22
Andy Hook <hello@andy-hook.co.uk>
2020-10-15 13:59:06 +0100

    Environment: Fix vars

--- src/environment/variables.ts
+++ src/environment/variables.ts
@@ -3,23 +3,22 @@ import { EnvNetworkName, EnvVariables } from './types'
 const DEFAULT_NETWORK_ENVIRONMENT: EnvNetworkName = 'rinkeby'
 
 export const ENV_VARS: EnvVariables = {
-  BUILD() {
-    return process.env.BUILD || 'undefined'
-  },
   NETWORK_ENVIRONMENT() {
-    return process.env.NETWORK_ENVIRONMENT || DEFAULT_NETWORK_ENVIRONMENT
+    return (
+      process.env.REACT_APP_NETWORK_ENVIRONMENT || DEFAULT_NETWORK_ENVIRONMENT
+    )
   },
   IPFS_GATEWAY() {
-    return process.env.IPFS_GATEWAY || ''
+    return process.env.REACT_APP_IPFS_GATEWAY || ''
   },
   FORTMATIC_API_KEY() {
-    return process.env.FORTMATIC_API_KEY || ''
+    return process.env.REACT_APP_FORTMATIC_API_KEY || ''
   },
   PORTIS_DAPP_ID() {
-    return process.env.PORTIS_DAPP_ID || ''
+    return process.env.REACT_APP_PORTIS_DAPP_ID || ''
   },
   SENTRY_DSN() {
-    const dsn = process.env.SENTRY_DSN || ''
+    const dsn = process.env.REACT_APP_SENTRY_DSN || ''
     return dsn.trim()
   },
 }
