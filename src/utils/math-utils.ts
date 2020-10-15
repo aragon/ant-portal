665ef5acfacc9849586b8509bf44826eeeefa3bd
Andy Hook <hello@andy-hook.co.uk>
2020-10-15 19:26:28 +0100

    Balances: Poll for token price

--- src/utils/math-utils.ts
+++ src/utils/math-utils.ts
@@ -5,7 +5,7 @@ export function bigNum(value: string | number): BigNumber {
 }
 
 /**
- * Format a decimal-based number back to a normal number
+ * Format a decimal-based number back to a big number
  *
  * @param {string} value the number
  * @param {number} digits number of decimal places
@@ -17,4 +17,24 @@ export function parseUnits(value: string, digits: number): BigNumber {
   } catch (err) {
     return bigNum(-1)
   }
+}
+
+/**
+ * Round a value
+ *
+ * @param {String} value Value to round
+ * @param {Number} precision Rounding precision
+ * @returns {String} Value rounded to `precision` decimals
+ */
+export function round(value: string, precision = 2): string {
+  const [whole, decimal] = value.split('.')
+
+  if (!decimal || decimal.length <= precision) return value
+
+  // Round and keep the last `precision` digits
+  const preciseDecimal = Math.round(
+    parseInt((decimal || '0').slice(0, precision + 2)) / 100
+  )
+
+  return `${whole}${preciseDecimal ? `.${preciseDecimal}` : ''}`
 }
