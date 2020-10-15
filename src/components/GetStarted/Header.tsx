c647397adf7b5a9cdebff810dc5e1c9486ddbacb
Andy Hook <hello@andy-hook.co.uk>
2020-10-15 20:30:18 +0100

    Home: Responsive adjustment

--- src/components/GetStarted/Header.tsx
+++ src/components/GetStarted/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback } from 'react'
 import { useHistory } from 'react-router-dom'
 // @ts-ignore
-import { GU, IconArrowRight, useTheme, useViewport } from '@aragon/ui'
+import { GU, IconArrowRight, useTheme, useLayout } from '@aragon/ui'
 import { fontWeight } from '../../style/font'
 import BrandButton from '../BrandButton/BrandButton'
 import LayoutLimiter from '../Layout/LayoutLimiter'
@@ -9,8 +9,9 @@ import LayoutLimiter from '../Layout/LayoutLimiter'
 function Header(): JSX.Element {
   const history = useHistory()
   const theme = useTheme()
-  const { below } = useViewport()
-  const compactMode = below('medium')
+
+  const { layoutName } = useLayout()
+  const compactMode = layoutName === 'small'
 
   const handleNavigateToConverter = useCallback(() => {
     history.push('/ant')
@@ -46,14 +47,15 @@ function Header(): JSX.Element {
             line-height: 1.2;
             font-size: 52px;
             margin-bottom: ${2.5 * GU}px;
+            font-size: ${compactMode ? `44` : `52`}px;
           `}
         >
-          Migrate to ANT v2
+          Migrate to ANT&nbsp;v2
         </h1>
         <p
           css={`
             font-weight: ${fontWeight.medium};
-            font-size: ${compactMode ? `20` : `26`}px;
+            font-size: ${compactMode ? `22` : `26`}px;
             color: ${theme.contentSecondary};
             margin: auto;
             margin-bottom: ${4 * GU}px;
@@ -61,7 +63,7 @@ function Header(): JSX.Element {
           `}
         >
           Use Aragon Migrate system to upgrade your ANT balance to the newest
-          version of the token contract.
+          version of the token&nbsp;contract.
         </p>
         <BrandButton
           mode="strong"
