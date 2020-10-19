609816861ee6aeab6266026295f1d068985e3524
Andy Hook <hello@andy-hook.co.uk>
2020-10-19 16:52:21 +0100

    SigningInfo: Code style and tidy

--- src/components/Migrate/Converter/SigningInfo.tsx
+++ src/components/Migrate/Converter/SigningInfo.tsx
@@ -3,42 +3,19 @@ import React, { ReactNode } from 'react'
 import { Link, Info, useTheme } from '@aragon/ui'
 
 type InfoStatus = 'working' | 'error' | 'success'
-
 type Descriptions = Record<InfoStatus, ReactNode>
-
-type SigningInfoProps = {
-  status: InfoStatus
-  multipleTransactions: boolean
-}
 
 const SINGLE_TX_DESCRIPTIONS: Descriptions = {
   working: (
     <>
-      Open your Ethereum provider (
-      <Link
-        href="https://metamask.io/"
-        css={`
-          text-decoration: none;
-        `}
-      >
-        Metamask
-      </Link>{' '}
-      or similar) to sign the transaction. Do not close this window until the
-      process has finished.
+      Open your Ethereum provider (<MetamaskLink /> or similar) to sign the
+      transaction. Do not close this window until the process has finished.
     </>
   ),
   error: (
     <>
       An error has occurred at the time of transaction. You can increase your
-      gas settings and try again.{' '}
-      <Link
-        href="https://metamask.zendesk.com/hc/en-us/articles/360015488771-How-to-Adjust-Gas-Price-and-Gas-Limit-"
-        css={`
-          text-decoration: none;
-        `}
-      >
-        How to adjust gas price and gas limit?
-      </Link>
+      gas settings and try again. <GasPriceInfoLink />
     </>
   ),
   success:
@@ -48,35 +25,23 @@ const SINGLE_TX_DESCRIPTIONS: Descriptions = {
 const MULTI_TX_DESCRIPTIONS: Descriptions = {
   working: (
     <>
-      Open your Ethereum provider (
-      <Link
-        href="https://metamask.io/"
-        css={`
-          text-decoration: none;
-        `}
-      >
-        Metamask
-      </Link>{' '}
-      or similar) to sign the transactions. Do not close this window until the
-      process has finished.
+      Open your Ethereum provider (<MetamaskLink /> or similar) to sign the
+      transactions. Do not close this window until the process has finished.
     </>
   ),
   error: (
     <>
       An error has occurred with one of the transactions. You can increase your
-      gas settings and try again.{' '}
-      <Link
-        href="https://metamask.zendesk.com/hc/en-us/articles/360015488771-How-to-Adjust-Gas-Price-and-Gas-Limit-"
-        css={`
-          text-decoration: none;
-        `}
-      >
-        How to adjust gas price and gas limit?
-      </Link>
+      gas settings and try again. <GasPriceInfoLink />
     </>
   ),
   success:
     'Success! The transactions have been sent to the network for processing. You can now review other migration options.',
+}
+
+type SigningInfoProps = {
+  status: InfoStatus
+  multipleTransactions: boolean
 }
 
 function SigningInfo({
@@ -104,6 +69,32 @@ function SigningInfo({
     >
       {description}
     </Info>
+  )
+}
+
+function MetamaskLink() {
+  return (
+    <Link
+      href="https://metamask.io/"
+      css={`
+        text-decoration: none;
+      `}
+    >
+      Metamask
+    </Link>
+  )
+}
+
+function GasPriceInfoLink() {
+  return (
+    <Link
+      href="https://metamask.zendesk.com/hc/en-us/articles/360015488771-How-to-Adjust-Gas-Price-and-Gas-Limit-"
+      css={`
+        text-decoration: none;
+      `}
+    >
+      How to adjust gas price and gas limit?
+    </Link>
   )
 }
 
