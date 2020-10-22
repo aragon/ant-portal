5d32f3555ffda027bc8c6fdd7c6d82fdce3893a0
delfipolito <delfinapolito@gmail.com>
2020-10-21 21:41:55 -0300

    Notification for mobile users to go to desktop (#51)

--- src/components/GetStarted/GetStarted.tsx
+++ src/components/GetStarted/GetStarted.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 // @ts-ignore
-import { GU } from '@aragon/ui'
+import { GU, Info } from '@aragon/ui'
 import Features from './Features'
 import LayoutGutter from '../Layout/LayoutGutter'
 import Header from './Header'
 import Balances from './Balances'
 import Faqs from './Faqs'
+import useDetectMobileDevice from '../../hooks/useDetectMobileDevice'
 
 function GetStarted(): JSX.Element {
+  const isMobile = useDetectMobileDevice()
   return (
     <LayoutGutter>
       <div
@@ -17,6 +19,18 @@ function GetStarted(): JSX.Element {
         `}
       >
         <Header />
+        {isMobile && (
+          <Info
+            css={`
+              max-width: ${62 * GU}px;
+              margin: ${4 * GU}px auto;
+            `}
+          >
+            ANT v2 Migration is not supported for mobile wallets. Use a web or
+            hardware wallet to interact with your account and begin the
+            migration.{' '}
+          </Info>
+        )}
         <Balances
           css={`
             padding-top: ${14 * GU}px;
