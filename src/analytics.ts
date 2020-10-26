1abe70e1bbc54668f053dad33c750cf66b8c99de
Andy Hook <hello@andy-hook.co.uk>
2020-10-26 11:59:34 +0000

    Analytics: Add minimal config

---
+++ src/analytics.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+// @ts-ignore
+export function trackEvent(event, options): void {
+  // @ts-ignore
+  const countly = window.Countly
+  if (countly?.q) {
+    // See https://support.count.ly/hc/en-us/articles/360037441932-Web-analytics-JavaScript-#custom-events
+    countly.q.push([
+      'add_event',
+      {
+        key: event,
+        count: 1,
+        ...options,
+      },
+    ])
+  }
+}
