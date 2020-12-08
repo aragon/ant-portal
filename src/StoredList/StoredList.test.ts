315260df1839b259a1017dfee372f8dd9b68f3d3
Andy Hook <hello@andy-hook.co.uk>
2020-12-08 12:41:55 +0000

    StoredList: Isolate test cases

--- src/StoredList/StoredList.test.ts
+++ src/StoredList/StoredList.test.ts
@@ -8,26 +8,38 @@ const MOCK_ITEM2 = {
   description: 'mock description 2',
 }
 
-describe('StoredList.', () => {
-  const list = new StoredList<{ description: string }>('key')
-  const spyLocalStorage = jest.spyOn(Storage.prototype, 'setItem')
+function createStoredList(key: string) {
+  return new StoredList<{ description: string }>(key)
+}
 
-  it('update() updates the list and stores in localStorage', () => {
-    list.update([MOCK_ITEM])
-    expect(list.getItems()).toEqual([MOCK_ITEM])
+describe('StoredList.', () => {
+  it('update() replaces the list and stores in localStorage', () => {
+    const list = createStoredList('update-test')
+    const spyLocalStorage = jest.spyOn(Storage.prototype, 'setItem')
+
+    list.update([MOCK_ITEM2])
+    list.update([MOCK_ITEM, MOCK_ITEM2])
+
+    expect(list.getItems()).toEqual([MOCK_ITEM, MOCK_ITEM2])
     expect(spyLocalStorage).toBeCalledWith(
-      'key',
-      '[{"description":"mock description 1"}]'
+      'update-test',
+      '[{"description":"mock description 1"},{"description":"mock description 2"}]'
     )
   })
 
   it('add() correctly adds an item to the list', () => {
+    const list = createStoredList('add-test')
+
+    list.add(MOCK_ITEM)
     list.add(MOCK_ITEM2)
 
     expect(list.getItems()).toEqual([MOCK_ITEM, MOCK_ITEM2])
   })
 
   it('remove() removes an item from the list via index', () => {
+    const list = createStoredList('remove-test')
+
+    list.update([MOCK_ITEM, MOCK_ITEM2])
     list.remove(1)
     expect(list.getItems()).toEqual([MOCK_ITEM])
   })
