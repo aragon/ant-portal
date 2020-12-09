b0fa374f32a1dd34b2c88f24adf0e0d13d91965c
Andy Hook <hello@andy-hook.co.uk>
2020-12-09 16:02:32 +0000

    Expand header and footer to fit larger screens

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,6 @@ import React, { ReactNode, useCallback } from 'react'
 import { GU, Link, useTheme, useLayout } from '@aragon/ui'
 import FooterLogo from './FooterLogo'
 import LayoutGutter from '../Layout/LayoutGutter'
-import LayoutLimiter from '../Layout/LayoutLimiter'
 import { useHistory, useLocation } from 'react-router-dom'
 import { DISCLAIMER_PATH } from '../../Routes'
 
@@ -28,38 +27,34 @@ function Footer(): JSX.Element {
   return (
     <footer>
       <LayoutGutter>
-        <LayoutLimiter>
+        <div
+          css={`
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            flex-direction: ${compactMode ? `column` : `row`};
+            padding-top: ${4 * GU}px;
+            padding-bottom: ${4 * GU}px;
+            border-top: 1px solid ${theme.border};
+          `}
+        >
           <div
             css={`
-              display: flex;
-              justify-content: space-between;
-              align-items: center;
-              flex-direction: ${compactMode ? `column` : `row`};
-              padding-top: ${4 * GU}px;
-              padding-bottom: ${4 * GU}px;
-              border-top: 1px solid ${theme.border};
+              display: inline-grid;
+              grid-auto-flow: column;
+              grid-gap: ${compactMode ? 3 * GU : 5 * GU}px;
+
+              margin-bottom: ${compactMode ? 2 * GU : `0`}px;
             `}
           >
-            <div
-              css={`
-                display: inline-grid;
-                grid-auto-flow: column;
-                grid-gap: ${compactMode ? 3 * GU : 5 * GU}px;
-
-                margin-bottom: ${compactMode ? 2 * GU : `0`}px;
-              `}
-            >
-              <FooterLink href={DOCS_URL}>Documentation</FooterLink>
-              <FooterLink onClick={handleDisclaimerClick}>
-                Disclaimer
-              </FooterLink>
-              <FooterLink href={COMMUNITY_URL}>Community</FooterLink>
-            </div>
-            <Link href={ARAGON_WEBSITE_URL}>
-              <FooterLogo />
-            </Link>
+            <FooterLink href={DOCS_URL}>Documentation</FooterLink>
+            <FooterLink onClick={handleDisclaimerClick}>Disclaimer</FooterLink>
+            <FooterLink href={COMMUNITY_URL}>Community</FooterLink>
           </div>
-        </LayoutLimiter>
+          <Link href={ARAGON_WEBSITE_URL}>
+            <FooterLogo />
+          </Link>
+        </div>
       </LayoutGutter>
     </footer>
   )
