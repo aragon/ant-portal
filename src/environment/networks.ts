7befa6e6694d18fab22cc8f97e0185869b3f42f0
nivida <samuel@ethereum.org>
2021-02-08 19:27:20 +0100

    contract addresses added in environment/networks

--- src/environment/networks.ts
+++ src/environment/networks.ts
@@ -13,8 +13,8 @@ const networks = new Map<EnvNetworkName, EnvNetworkConfig>([
       contracts: {
         tokenAntV1: '0x960b236A07cf122663c4303350609A66A7B288C0',
         tokenAntV2: '0xa117000000f279D81A1D3cc75430fAA017FA5A2e',
-        tokenAnj: '', // TODO: Add the address once ANJ is integrated
-        anjMigrator: '', // TODO: Add the address once ANJ is integrated
+        tokenAnj: '0xcD62b1C403fa761BAadFC74C525ce2B51780b184',
+        anjMigrator: '0xf6271c8eBF1C1C0384CBBD6Df8b84380623555Ef',
         migrator: '0x078BEbC744B819657e1927bF41aB8C74cBBF912D',
         antEthUniswapPool: '0xfa19de406e8f5b9100e4dd5cad8a503a6d686efe',
         antUniIncentivePool: '0xea4d68cf86bce59bf2bfa039b97794ce2c43debc',
