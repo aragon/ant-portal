878bf6403b6b9c8a3284a7eef0f3c1be11348dcb
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-09-02 11:23:22 +0200

    cleared options data

--- src/token-info/options.ts
+++ src/token-info/options.ts
@@ -3,6 +3,7 @@ export type TxHash = string
 
 // Add tx hashes (values) for DAOs (keys) that received their options.
 export const optionsInfo: Record<DaoAddress, TxHash> = {
-  '0x8367dc645e31321CeF3EeD91a10a5b7077e21f70':
-    '0x7b6f030dbbf8ee4e22488b4edf239f0e9cdbaa54fcef985c81d324bd3cbccffb',
+  // this is an example entry:
+  // '0x8367dc645e31321CeF3EeD91a10a5b7077e21f70':
+  //   '0x7b6f030dbbf8ee4e22488b4edf239f0e9cdbaa54fcef985c81d324bd3cbccffb',
 }
